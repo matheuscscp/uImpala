0d4b8572265236d9eb5377e486fd491ecef6269a
Pimenta <matheuscscp@gmail.com>
2014-02-24 20:24:45 -0300

    Extracting GameSettings from UbiGame

---
+++ src/org/unbiquitous/ubiengine/engine/core/GameSettings.java
@@ -0,0 +1,23 @@
+package org.unbiquitous.ubiengine.engine.core;
+
+import java.util.HashMap;
+
+/**
+ * Just a "typedef" for
+ * <code>HashMap{@literal <}String, Object{@literal >}</code>.
+ * @author Pimenta
+ *
+ */
+@SuppressWarnings("serial")
+public final class GameSettings extends HashMap<String, Object> {
+  /**
+   * Engine's private use.
+   */
+  protected GameSettings validate() {
+    if (get("root_path") == null)
+      put("root_path", ".");
+    if (get("first_state") == null)
+      throw new Error("First game state not defined!");
+    return this;
+  }
+}
