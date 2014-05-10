31a95084e285019148253be42b9b2c02df81f54f
Pimenta <matheuscscp@gmail.com>
2014-05-10 10:55:16 -0300

    Adding extends of time dalkvik impl

--- src/org/unbiquitous/uImpala/dalvik/impl/time/Time.java
+++ src/org/unbiquitous/uImpala/dalvik/impl/time/Time.java
@@ -1,5 +1,11 @@
 package org.unbiquitous.uImpala.dalvik.impl.time;
 
-public class Time {
+public class Time extends org.unbiquitous.uImpala.engine.time.Time {
+
+  @Override
+  protected long getTime() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
   
 }
