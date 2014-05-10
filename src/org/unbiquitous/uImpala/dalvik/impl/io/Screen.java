8faf8ec8afa7ba0ae88ca6dd50d0a5635f1ecb9f
Pimenta <matheuscscp@gmail.com>
2014-05-10 10:59:09 -0300

    Adding extends of io dalvik impl

--- src/org/unbiquitous/uImpala/dalvik/impl/io/Screen.java
+++ src/org/unbiquitous/uImpala/dalvik/impl/io/Screen.java
@@ -1,5 +1,116 @@
 package org.unbiquitous.uImpala.dalvik.impl.io;
 
-public class Screen {
+import org.unbiquitous.uImpala.engine.io.KeyboardSource;
+import org.unbiquitous.uImpala.engine.io.MouseSource;
+
+public class Screen extends org.unbiquitous.uImpala.engine.io.Screen {
+
+  @Override
+  public void open(String t, int w, int h, boolean f, String i, boolean gl) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public String getTitle() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void setTitle(String title) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public int getWidth() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public int getHeight() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public void setSize(int width, int height) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public boolean isFullscreen() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public void setFullscreen(boolean fullscreen) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public String getIcon() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void setIcon(String icon) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public boolean isCloseRequested() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public MouseSource getMouse() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public KeyboardSource getKeyboard() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void start() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void stop() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  protected void update() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void close() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public boolean isUpdating() {
+    // TODO Auto-generated method stub
+    return false;
+  }
   
 }
--- src/org/unbiquitous/uImpala/dalvik/impl/io/Speaker.java
+++ src/org/unbiquitous/uImpala/dalvik/impl/io/Speaker.java
@@ -1,5 +1,35 @@
 package org.unbiquitous.uImpala.dalvik.impl.io;
 
-public class Speaker {
+public class Speaker extends org.unbiquitous.uImpala.engine.io.Speaker {
+
+  @Override
+  public void start() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void stop() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  protected void update() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void close() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public boolean isUpdating() {
+    // TODO Auto-generated method stub
+    return false;
+  }
   
 }
