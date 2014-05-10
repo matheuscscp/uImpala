a61d3493a77d52687dda625893f571a2602b6d35
Pimenta <matheuscscp@gmail.com>
2014-05-10 10:54:15 -0300

    Adding extends on core dalvik impl

--- src/org/unbiquitous/uImpala/dalvik/impl/core/Game.java
+++ src/org/unbiquitous/uImpala/dalvik/impl/core/Game.java
@@ -1,5 +1,11 @@
 package org.unbiquitous.uImpala.dalvik.impl.core;
 
-public class Game {
+public class Game extends org.unbiquitous.uImpala.engine.core.Game {
+
+  @Override
+  protected void initImpl() {
+    // TODO Auto-generated method stub
+    
+  }
   
 }
